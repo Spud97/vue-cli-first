4bffe696b79fae8e4c3ecc4a0ed49425136b0a94
Spud97 <luca.dercole97@gmail.com>
2022-06-20 18:12:55 +0200

    Eserczio svolto

--- src/App.vue
+++ src/App.vue
@@ -26,3 +26,31 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+<template>
+  <div id="Title">
+    <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+import Title from './components/HelloWorld.vue'
+
+export default {
+  name: 'Title',
+  components: {
+    Title
+  }
+}
+</script>
+
+<style lang="scss">
+#Title {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
